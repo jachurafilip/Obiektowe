5b6abfa49678b4a212b828de02b0a29c81e5dc71
Filip <filjac5882@op.pl>
2018-10-14 21:13:18 +0200

    Pesel

---
+++ src/lab1/pesel/PESELTests.java
@@ -0,0 +1,14 @@
+package lab1.pesel;
+
+import java.util.Scanner;
+
+public class PESELTests {
+    public static void main(String[] args){
+        String pesel;
+		System.out.println("Podaj pesel");
+		Scanner read = new Scanner(System.in);
+		pesel = read.nextLine();
+		if(PESEL.check(pesel)) System.out.println("Dobry pesel");
+		else System.out.println("Zly pesel");
+        }
+    }
