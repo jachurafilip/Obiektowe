3f9f9d37b4505d608db5ccc3aee239ddcf6d63e8
filip <filjac5882@op.pl>
2018-10-23 15:59:36 +0200

    Mail is succesfully sent

--- src/lab3/Email/main.java
+++ src/lab3/Email/main.java
@@ -7,23 +7,29 @@ import javax.mail.internet.*;
 public class main {
     public static void main(String[] args)
     {
-        Properties prop = new Properties();
-        prop.put("mail.transport.protocol","smtp");
-        prop.put("mail.smtp.host","smtp.poczta.onet.pl");
-        prop.put("mail.smtp.port", "465");
-        prop.put("mail.smtp.auth","false");
 
-        Authenticator auth = new Authenticator()
-        {
-            protected PasswordAuthentication getPasswordAuthentication()
-            {
-                return new PasswordAuthentication("filip.jachura@onet.eu","filip12");
-            }
-        };
+        final String username = "ObiektoweMail";
+        final String password = "Obiektowe1";
 
-        Session session = Session.getDefaultInstance(prop,auth);
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", "smtp.gmail.com");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
 
-    EmailMessage msg = EmailMessage.builder().setFrom("filip.jachura@onet.eu").setTo("filjac5882@op.pl").setSubject("a").setContent("b").build();
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+    EmailMessage msg = EmailMessage.builder().setFrom("ObiektoweMail@gmail.com").setTo("obiektowe@niepodam.pl").setSubject("a").setContent("b").build();
 
     try
     {
@@ -31,6 +37,12 @@ public class main {
         message.setFrom(new InternetAddress(msg.getFrom()));
         for(var a: msg.getTo()) {
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(a));
+        }
+        for(var a: msg.getCc()) {
+            message.addRecipient(Message.RecipientType.CC, new InternetAddress(a));
+        }
+        for(var a: msg.getBcc()) {
+            message.addRecipient(Message.RecipientType.BCC, new InternetAddress(a));
         }
         message.setSubject(msg.getSubject());
         message.setText(msg.getContent());
@@ -44,6 +56,6 @@ public class main {
         ex.printStackTrace();
     }
 
-}
+    }
 }
 
