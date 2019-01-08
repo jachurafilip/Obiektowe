5945ef970c5c91e4f5950bae38234f5eb54259c2
filip <filjac5882@op.pl>
2019-01-08 09:59:32 +0100

    final

--- src/lab10/src/sample/Server.java
+++ src/lab10/src/sample/Server.java
@@ -1,4 +1,87 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
+    private ServerSocket        socket;
+    private ConnectionListener  connectionListener;
+
+    // temp
+    private List<Client> clientList = new ArrayList<Client>();
+    // temp end
+
+    public Server(int port) {
+        try {
+            socket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        connectionListener = new ConnectionListener(this);
+    }
+
+    public void start() throws IOException {
+
+        connectionListener.start();
+
+        // temp will move to a Thread later
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while (((input = stdIn.readLine()) != null) && connectionListener.isAlive()) {
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            } else {
+                for (int i = 0; i < input.length(); i++)
+                    System.out.print("\b");
+                System.out.println("Admin: " + input);
+                for (Client c : clientList) {
+                    c.send("Admin: " + input);
+                }
+            }
+
+        }
+        stop();
+        // temp end
+    }
+
+    public void stop() {
+
+        connectionListener.stop();
+        for (Client c : clientList) {
+            c.closeSession();
+        }
+
+        System.out.println("Server terminated!");
+    }
+
+    public synchronized void addConnection(Connection connection) {
+
+        Client c = new Client(connection, clientList);
+        clientList.add(c);
+        c.startSession();
+        System.out.println("Client connected");
+    }
+
+    public ServerSocket getSocket() {
+
+        return socket;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int port;
+        if (args.length > 0)
+            port = Integer.parseInt(args[0]);
+        else
+            port = 4444;
+        Server s = new Server(port);
+        s.start();
+    }
+
 }
--- src/lab8/src/books/DB.java
+++ src/lab8/src/books/DB.java
@@ -1,4 +1,4 @@
-package books;
+	package books;
 import java.sql.*;
 import java.util.ArrayList;
 public class DB {
